e54d933221511600969b8a83a2200fb0012399c8
even904 <1764269411@qq.com>
2025-10-03 17:29:03 +0800

    ✔ Update posts: Transfer layer

--- generate_md_template.py
+++ generate_md_template.py
@@ -30,8 +30,8 @@ tags: {tags_str}
 author = "Even"
 initial_date = datetime.now().strftime("%Y年%m月%d日")
 update_date = datetime.now().strftime("%Y年%m月%d日")
-title = "【知识框架】Docker知识体系：从基础原理到云原生生态"
-tags = ["Docker,容器化,镜像与容器,多架构支持,网络模式,数据管理,Docker Compose,Kubernetes,CI/CD,云原生"]
+title = "【计算机网络】3 传输层"
+tags = ["计算机网络, 传输层, TCP, UDP"]
 
 # 生成Markdown模板
 markdown_content = generate_markdown_template(title, author, tags, initial_date, update_date)
