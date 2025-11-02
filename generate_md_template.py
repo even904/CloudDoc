f9a2e975aff65e6e599664174fefc50cba54c557
even904 <1764269411@qq.com>
2025-11-03 00:21:33 +0800

    ✔ Update posts

--- generate_md_template.py
+++ generate_md_template.py
@@ -30,8 +30,8 @@ tags: {tags_str}
 author = "Even"
 initial_date = datetime.now().strftime("%Y年%m月%d日")
 update_date = datetime.now().strftime("%Y年%m月%d日")
-title = "【计算机网络】实践-2：ssh跨网转发"
-tags = ["计算机网络, CVM, 端口转发"]
+title = "【工业互联网】2 通感算控(CPCC)技术前瞻"
+tags = ["网络, AI, 边缘, 云, 具身智能"]
 
 # 生成Markdown模板
 markdown_content = generate_markdown_template(title, author, tags, initial_date, update_date)
