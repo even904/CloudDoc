213ed932501c2ac481e0e00c98e106bd8e461e9e
even904 <1764269411@qq.com>
2026-03-21 01:39:06 +0800

    ✔ Update posts

--- generate_md_template.py
+++ generate_md_template.py
@@ -30,8 +30,8 @@ tags: {tags_str}
 author = "Even"
 initial_date = datetime.now().strftime("%Y年%m月%d日")
 update_date = datetime.now().strftime("%Y年%m月%d日")
-title = "【强化学习】6 随机近似与随机梯度下降"
-tags = ["强化学习,数学原理,随机近似,随机梯度下降,Robbins-Monro"]
+title = "【强化学习】7 时序差分方法"
+tags = ["强化学习,数学原理,Temporal-Difference,时序差分"]
 
 # 生成Markdown模板
 markdown_content = generate_markdown_template(title, author, tags, initial_date, update_date)
